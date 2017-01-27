ce19e0fd9f9afc3e4781f5343f78399c19b3cfdc
Eddy Huleu <eddy.huleu@live.fr>
2017-01-27 15:58:10 +0100

    javascript echec

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded',    function(){
+    
+    var textDiv = document.getElementById('CAMARCHEEEPASSSSSSSS');
+    document.getElementById('CAMARCHEEEEPASSSSSSSSSS').addEventListener('click',function(){
+        var words = textDiv.innerText.split(' ');
+        var len = words.length;
+        var result = '';
+        textDiv.innerText = '';
+        for(var i = 0; i < len; ++i){
+            var randIndex = Math.floor((Math.random() * (words.length)));
+            result += words[randIndex] + ' ';
+            words.splice(randIndex, 1);
+        }
+        textDiv.innerText = result;
+    }, false);
+
+}, false)   
